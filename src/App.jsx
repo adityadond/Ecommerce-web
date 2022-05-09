3e109f9ad1abe44a9d6eef952109a8129ae83469
unknown <adityadond281@gmail.com>
2022-05-09 15:52:38 +0530

    added multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+// import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <div><Home/></div>;
+  return <div><Cart/></div>;
 };
 
 export default App;
